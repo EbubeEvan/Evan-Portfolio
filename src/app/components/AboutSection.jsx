d9d2b18717bccfac0ee0e611db16decfbe688685
EbubeEvan <ebubemmayie@gmail.com>
2024-01-03 23:46:06 +0100

    fixed about-me margin on mobile

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -71,7 +71,7 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-white" id="about">
+    <section className="text-white max-md:mt-[5rem]" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <motion.div initial="hidden"
           whileInView="visible"
