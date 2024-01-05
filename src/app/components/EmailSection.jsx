cc26e05504563a83b153eee7a0489000cc71fddc
EbubeEvan <ebubemmayie@gmail.com>
2024-01-05 18:45:33 +0100

    added form submit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -11,30 +11,8 @@ const EmailSection = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
-
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-
-    if (response.status === 200) {
-      console.log("Message sent.");
       setEmailSubmitted(true);
     }
-  };
 
   return (
     <section
@@ -83,7 +61,7 @@ const EmailSection = () => {
             Email sent successfully!
           </p>
         ) : (
-          <form className="flex flex-col" onSubmit={handleSubmit}>
+          <form className="flex flex-col" action="https://formsubmit.co/ebubemmayie@gmail.com" method="POST" onSubmit={handleSubmit}>
             <div className="mb-6">
               <label
                 htmlFor="email"
