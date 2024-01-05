4e0731f43cd5a826eee734eda549d8840b7ba70b
EbubeEvan <ebubemmayie@gmail.com>
2024-01-05 15:54:58 +0100

    fixed email again

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,27 +1,23 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const myEmail = process.env.MY_EMAIL;
 
 export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
+  const { subject, message } = await req.json();
+  console.log(subject, message);
   try {
     const data = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
-      to: [email, myEmail],
+      to: ["ebubemmayie@gmail.com"],
       subject: subject,
       react: (
         <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
           <p>{message}</p>
         </>
       ),
     });
+    console.log(data)
     console.log(NextResponse.json(data));
     return NextResponse.json(data);
   } catch (error) {
