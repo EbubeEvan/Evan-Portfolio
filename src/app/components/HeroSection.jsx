4b74248c71def9fc32320f13d52cdcfa6aa4ef81
EbubeEvan <ebubemmayie@gmail.com>
2024-01-09 23:43:31 +0100

    added cv download

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
-import Link from "next/link";
 
 const HeroSection = () => {
   return (
@@ -36,20 +35,21 @@ const HeroSection = () => {
            Frontend developer with proficiency in creating user-friendly and responsive web applications.
           </p>
           <div>
-            <Link
+            <a
               href="#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               Hire Me
-            </Link>
-            {/* <Link
-              href="/"
+            </a>
+            <a
+              href="/Evangeline-Mmayie-CV.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              download=''
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
-            </Link> */}
+            </a>
           </div>
         </motion.div>
         <motion.div
